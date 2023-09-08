3d0a498e1e9475b3d930c2ca538c5b971b4cd074
AmulyaDukunta <amulya.d@beehyv.com>
2023-09-08 19:02:15 +0530

    animation

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function countUp() {
         // Round up and set counter value
         counter.innerText = `${Math.ceil(c + increment)}`;
 
-        setTimeout(updateCounter, 18);
+        setTimeout(updateCounter, 30);
       } else {
         counter.innerText = target;
       }
@@ -77,5 +77,29 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
+let rocketPieces = document.querySelectorAll('.rocket-body span')
 
+let rocket = document.querySelector('.rocket')
+
+let triggerStart = window.innerHeight/5;
+
+let rocketOffsetTop = rocket.offsetTop;
+
+let thirdOffsetTop = rocketPieces[2].offsetTop;
+
+document.addEventListener('scroll', (e) => {
+  if(window.scrollY > (rocketOffsetTop - triggerStart)) {
+    rocketPieces[0].classList.add('active');
+    rocketPieces[1].classList.add('active');
+  } else {
+    rocketPieces[0].classList.remove('active');
+    rocketPieces[1].classList.remove('active');
+  }
+
+  if(window.scrollY > (thirdOffsetTop - triggerStart)) {
+    rocketPieces[2].classList.add('active');
+  } else {
+    rocketPieces[2].classList.remove('active');
+  }
+})
 
